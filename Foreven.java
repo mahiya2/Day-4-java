8d46fd8fb7b863beaf0027f0b2db06854bf4fa43
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-05 19:07:25 +0530

    Add files via upload

---
+++ Foreven.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Foreven{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int i=sc.nextInt();
+        for(i=0;i<=30;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
