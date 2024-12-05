3640add2f2235479af517f298c4cc6ee3793b460
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-05 19:29:40 +0530

    Add files via upload

---
+++ Forodd.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Forodd{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        for(i=1;i<=10;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
