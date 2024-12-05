cbb0ca658515c8fd7271a7ed5be16bc1e95c9faa
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-05 21:56:45 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class While{
+    public static void main(String args[]){
+     int i=10;
+    while(i>0)
+        {
+        System.out.println();
+        i-=1;
+        }
+    }
+}
