d5b4c613a323b6ef9656f337f91be7ea6b1483d3
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-05 12:44:36 +0530

    Add files via upload

---
+++ Midless.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Midless{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        if(a<b){
+            if(b<c)
+            System.out.println("b is small");
+            else if(a<c)
+            System.out.println("c is small");
+            else
+            System.out.println("a is smaller");
+        }
+        else
+        {
+            if(a<c)
+            System.out.println("a is smaller");
+            else if(b<c)
+            System.out.println("c is smaller");
+            else
+            System.out.println("b is smaller");
+        }
+    }
+}
